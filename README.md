9353e272faaccacd93089c60e4312a9a77c21c7e
Hoa <tnguyen568@my.bcit.ca>
2023-05-09 05:57:58 -0700

    Finished Assignment 1.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 2537_Assignment_1
+This is HoaNguyenThai's Comp2537 Assignment 1.
+Finished.
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const bcrypt = require('bcrypt'); //encrypting password
 const Joi = require("joi"); //checking matching password
 const saltRounds = 10;
 
-const expireTime = 1 * 60 * 60 * 1000; //expires after 1 day  (hours * minutes * seconds * millis)
+const expireTime = 1 * 60 * 60 * 1000; //expires after 1 hour (hours * minutes * seconds * millis)
 const port = process.env.PORT || 1520;
 
 /* secret stuff */
@@ -49,13 +49,14 @@ app.use(session({
 app.get('/', (req,res) => {
     if (!req.session.authenticated) {
         var html = `
+        Hey there :) <br><br>
         <button onclick="window.location.href='/signup'"> Sign up </button> <br>
         <button onclick="window.location.href='/login'"> Log in </button>
         `;
         res.send(html);
     } else {
         var html = `
-        Hello Name! <br>
+        Hello ${req.session.username}! <br><br>
         <button onclick="window.location.href='/members'"> Go to Members Area </button> <br>
         <button onclick="window.location.href='/logout'"> Logout </button>
         `;
@@ -65,38 +66,13 @@ app.get('/', (req,res) => {
 
 app.get('/signup', (req,res) => {
     var html = `
-    Signing up!
+    Signing up! <br><br>
     <form action='/signUpUser' method='post'>
         <input name='username' type='text' placeholder='username'> <br>
         <input name='email' type='email' placeholder='email'> <br>
         <input name='password' type='password' placeholder='password'> <br>
-    <button> Submit </button>
+        <button> Submit </button>
     </form>
-    `;
-    /*if (missingEmail) {
-        html = `
-        Name is required
-        <button onclick="window.location.href='/signUpUser'"> Retry </button>
-        `;
-    }*/
-    res.send(html);
-});
-
-app.get('/login', (req,res) => {
-    var html = `
-    Logging in!
-    <form action='/loginUser' method='post'>
-        <input name='username' type='text' placeholder='username'> <br>
-        <input name='password' type='password' placeholder='password'> <br>
-    <button> Submit </button>
-    </form>
-    `;
-    res.send(html);
-});
-
-app.get('/members', (req,res) => {
-    var html = `
-    Welcome!
     `;
     res.send(html);
 });
@@ -113,25 +89,110 @@ app.post('/signUpUser', async (req,res) => {
 			password: Joi.string().max(20).required()
 		}
     );
-	
+
 	const validationResult = schema.validate({username, email, password});
 	if (validationResult.error != null) {
 	   console.log(validationResult.message);
-	   return;
+       var html = `
+       ${validationResult.error.message} <br><br>
+       <button onclick="window.location.href='/signup'"> Retry </button>
+       `;
+    } else {
+        var hashedPassword = await bcrypt.hash(password, saltRounds);
+        await userCollection.insertOne({username: username, password: hashedPassword});
+        console.log("Inserted user");
+        res.redirect("/");
     }
 
-    var hashedPassword = await bcrypt.hash(password, saltRounds);
-	
-	await userCollection.insertOne({username: username, password: hashedPassword});
-	console.log("Inserted user");
+    res.send(html);
+});
 
-    res.redirect('/members');
+app.get('/login', (req,res) => {
+    var html = `
+    Logging in! <br><br>
+    <form action='/logInUser' method='post'>
+        <input name='username' type='text' placeholder='username'> <br>
+        <input name='password' type='password' placeholder='password'> <br>
+        <button> Submit </button>
+    </form>
+    `;
+    res.send(html);
+});
+
+app.post('/logInUser', async (req, res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    const schema = Joi.string().max(20).required();
+    const validationResult = schema.validate(username);
+    if (validationResult.error != null) {
+        console.log(validationResult.error);
+        var html = `
+            Username is not allowed to be empty! <br><br>
+            <button onclick="window.location.href='/login'"> Retry </button>
+            `;
+        res.send(html);
+        return;
+    }
+
+    const result = await userCollection.find({username: username}).project({username: 1, password: 1, _id: 1}).toArray();
+
+	if (result.length != 1) {
+		var html = `
+        User not found! <br><br>
+        <button onclick="window.location.href='/login'"> Retry </button>
+        `;
+        res.send(html);
+	}
+	else if (await bcrypt.compare(password, result[0].password)) {
+		console.log("correct password");
+		req.session.authenticated = true;
+		req.session.username = username;
+		req.session.cookie.maxAge = expireTime;
+
+		res.redirect('/');
+		return;
+	} else {
+		html = `
+        Incorrect password! <br><br>
+        <button onclick="window.location.href='/login'"> Retry </button>
+        `;
+        res.send(html);
+	}
+});
+
+app.get('/members', (req,res) => {
+    if (req.session.authenticated) {
+        const images = [
+            "Jacq.jpg",
+            "Miriam.jpg",
+            "Nemona.jpg"
+        ];
+        const randomIndex = Math.floor(Math.random() * 3);
+        const selectedImage = images[randomIndex];
+
+        var html = `
+        Hello ${req.session.username}! <br><br>
+        <img src='/${selectedImage}' style='width:500px' alt="Member's welcome picture!"> <br><br>
+        <button onclick="window.location.href='/logout'"> Logout </button>
+        `;
+        res.send(html);
+    } else {
+        res.redirect("/");
+    }
 });
 
 app.get('/logout', (req,res) => {
 	req.session.destroy();
     res.redirect('/');
 });
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("*", (req,res) => {
+	res.status(404);
+	res.send("Oops! Page not found - 404");
+})
 
 app.listen(port, () => {
 	console.log("Node application listening on port "+port);
